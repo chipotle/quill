01749f5f2064aeb48c9684ea12cc28fc339acf6c
Watts Martin <layotl@gmail.com>
2013-04-25 21:59:47 -0700

    Set up basic Blade templating

---
+++ app/views/hello.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title')
+Hello World!
+@endsection
+
+@section('content')
+<h1>Hello World!</h1>
+
+{{ $text }}
+@endsection
---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>@yield('title')</title>
+{{ Html::style('cnq.css') }}
+@yield('head')
+</head>
+
+<body>
+<div id="content">
+@yield('content')
+</div>
+</body>
+
+</html>
