c88ae59b7c6d76a835c2d6516f62970125c07b71
Watts Martin <layotl@gmail.com>
2013-04-30 09:23:20 -0700

    Commit editor config file (and a few space-to-tab nits)

--- app/models/Author.php
+++ app/models/Author.php
@@ -2,41 +2,41 @@
 
 class Author extends Eloquent {
 
-    /**
-     * Database table used by model
-     */
-    protected $table = 'authors';
+	/**
+	 * Database table used by model
+	 */
+	protected $table = 'authors';
 
-    /**
-     * Class Constants
-     */
+	/**
+	 * Class Constants
+	 */
 
-    const SHOW_NAME = 10;
-    const SHOW_NICK = 20;
-    const SHOW_BOTH = 30;
+	const SHOW_NAME = 10;
+	const SHOW_NICK = 20;
+	const SHOW_BOTH = 30;
 
-    /**
-     * Author belongsTo User
-     */
-        public function user()
-    {
-        return $this->belongsTo('User');
-    }
+	/**
+	 * Author belongsTo User
+	 */
+		public function user()
+	{
+		return $this->belongsTo('User');
+	}
 
-    /**
-     * Author hasMany Stories
-     */
-    function stories()
-    {
-        return $this->hasMany('Story');
-    }
+	/**
+	 * Author hasMany Stories
+	 */
+	function stories()
+	{
+		return $this->hasMany('Story');
+	}
 
-    /**
-     * Author hasMany Pitches
-     */
-    function pitches()
-    {
-        return $this->hasMany('Pitch');
-    }
+	/**
+	 * Author hasMany Pitches
+	 */
+	function pitches()
+	{
+		return $this->hasMany('Pitch');
+	}
 
 }
--- app/models/Pitch.php
+++ app/models/Pitch.php
@@ -2,40 +2,40 @@
 
 class Pitch extends Eloquent {
 
-    protected $table = 'pitches';
+	protected $table = 'pitches';
 
-    /**
-     * Class constants
-     */
-    const UNSEEN    = 10;
-    const REVIEW    = 20;
-    const REJECTED  = 30;
-    const ACCEPTED  = 40;
-    const WAITING   = 60;
-    const PUBLISHED = 70;
+	/**
+	 * Class constants
+	 */
+	const UNSEEN    = 10;
+	const REVIEW    = 20;
+	const REJECTED  = 30;
+	const ACCEPTED  = 40;
+	const WAITING   = 60;
+	const PUBLISHED = 70;
 
-    static public $statusList = [
-        Pitch::UNSEEN    => 'Unseen',
-        Pitch::REVIEW    => 'In Review',
-        Pitch::REJECTED  => 'Rejected',
-        Pitch::ACCEPTED  => 'Accepted',
-        Pitch::WAITING   => 'Waiting for Rev',
-        Pitch::PUBLISHED => 'Published'
-    ];
-    /**
-     * Pitch belongsTo Author
-     */
-    function author()
-    {
-        return $this->belongsTo('Author');
-    }
+	static public $statusList = [
+		Pitch::UNSEEN    => 'Unseen',
+		Pitch::REVIEW    => 'In Review',
+		Pitch::REJECTED  => 'Rejected',
+		Pitch::ACCEPTED  => 'Accepted',
+		Pitch::WAITING   => 'Waiting for Rev',
+		Pitch::PUBLISHED => 'Published'
+	];
+	/**
+	 * Pitch belongsTo Author
+	 */
+	function author()
+	{
+		return $this->belongsTo('Author');
+	}
 
-    /**
-     * Pitch belongsTo Story
-     */
-    function story()
-    {
-        return $this->belongsTo('Story');
-    }
+	/**
+	 * Pitch belongsTo Story
+	 */
+	function story()
+	{
+		return $this->belongsTo('Story');
+	}
 
 }
