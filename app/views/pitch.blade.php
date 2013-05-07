9e54ca4c39f5a8044a4c8bf1b5f75de610235606
Watts Martin <layotl@gmail.com>
2013-05-07 10:27:12 -0700

    Change pitch form wording

--- app/views/pitch.blade.php
+++ app/views/pitch.blade.php
@@ -7,7 +7,7 @@
 
 <h1>Pitch a story!</h1>
 
-<p>You&#8217;ve probably come to this page from the <a href="/page/guidelines">guideline page</a>, but if perchance you didn&#8217;t, you definitely want to review those to know what we&#8217;re looking for.</p>
+<p>You&#8217;ve probably come to this page from the <a href="/page/guidelines">guideline page</a>, but if perchance you didn&#8217;t, you definitely want to review those to know what we&#8217;re looking for. What we&#8217;re looking for <em>now</em> is a short description of your proposed article, the “elevator pitch” as to why it&#8217;s an interesting story to tell. (If it&#8217;s a review, this is easy&mdash;just tell us what you want to review!)</p>
 
 <p>If you&#8217;ve published with us before, please use the same email address as you did with your previous submission if you can&#8212;that helps us keep track of things a little better.</p>
 
@@ -19,7 +19,7 @@
 <p>{{ Form::label('name', 'Your name') }}<br>
 {{ Form::text('name') }}</p>
 
-<p>{{ Form::label('blurb', 'What’s your story idea?') }}<br>
+<p>{{ Form::label('blurb', 'Your story idea') }}<br>
 {{ Form::textarea('blurb') }}</p>
 
 <p>{{ Form::submit('Pitch') }}</p>
