99fbde23bb803084bb76d99eb5908510993b9b65
Watts Martin <layotl@gmail.com>
2013-07-18 15:38:57 -0700

    Remove Lato and hyphenate.js

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>@yield('title')</title>
-  <!-- <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,700"> -->
   <script type="text/javascript" src="//use.typekit.net/fky8rov.js"></script>
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
   {{ HTML::style('css/cnq.css') }}
@@ -29,5 +28,4 @@
     @yield('content')
   </div>
 </body>
-{{-- HTML::script('js/hyphenate.js') --}}
 </html>
