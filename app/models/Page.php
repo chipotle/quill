eb9a96f1e9231c3c09df825ab011e27d89633bfe
Watts Martin <layotl@gmail.com>
2013-07-25 16:02:52 -0700

    Correct inexplicable spaces

--- app/models/Page.php
+++ app/models/Page.php
@@ -3,22 +3,22 @@ use Michelf\MarkdownExtra, Chipotle\Smartypants;
 
 class Page extends Eloquent {
 
-    /**
-     * Database table used by model
-     */
-    protected $table = 'pages';
+	/**
+	 * Database table used by model
+	 */
+	protected $table = 'pages';
 
-    protected $fillable = ['title', 'slug', 'body', 'head', 'is_visible'];
+	protected $fillable = ['title', 'slug', 'body', 'head', 'is_visible'];
 
-    public function getContent()
-    {
-        $content = Cache::rememberForever("page-{$this->id}", function() {
-            $body = MarkdownExtra::defaultTransform($this->body);
-            $body = Smartypants::defaultTransform($body);
-            $title = Smartypants::defaultTransform($this->title);
-            return ['title' => $title, 'body' => $body,
-                    'head' => $this->head];
-        });
-        return $content;
-    }
+	public function getContent()
+	{
+		$content = Cache::rememberForever("page-{$this->id}", function() {
+			$body = MarkdownExtra::defaultTransform($this->body);
+			$body = Smartypants::defaultTransform($body);
+			$title = Smartypants::defaultTransform($this->title);
+			return ['title' => $title, 'body' => $body,
+					'head' => $this->head];
+		});
+		return $content;
+	}
 }
