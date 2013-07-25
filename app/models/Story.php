b0ae6656a94a922c2b2030c9dd4baf58f4fcce6c
Watts Martin <layotl@gmail.com>
2013-07-25 16:03:34 -0700

    Add getContent() method to Story with caching

--- app/models/Story.php
+++ app/models/Story.php
@@ -2,35 +2,61 @@
 
 class Story extends Eloquent {
 
-    /**
-     * Database table used by model
-     */
-    protected $table = 'stories';
+	/**
+	 * Database table used by model
+	 */
+	protected $table = 'stories';
 
-    protected $guarded = ['id'];
+	protected $guarded = ['id'];
 
-    /**
-     * Story belongsTo Issue
-     */
-    function issue()
-    {
-        return $this->belongsTo('Issue');
-    }
+	/**
+	 * Story belongsTo Issue
+	 */
+	function issue()
+	{
+		return $this->belongsTo('Issue');
+	}
 
-    /**
-     * Story belongsTo Author
-     */
-    function author()
-    {
-        return $this->belongsTo('Author');
-    }
+	/**
+	 * Story belongsTo Author
+	 */
+	function author()
+	{
+		return $this->belongsTo('Author');
+	}
 
-    /**
-     * Story hasOne Pitch
-     */
-    function pitch()
-    {
-        return $this->hasOne('Pitch');
-    }
+	/**
+	 * Story hasOne Pitch
+	 */
+	function pitch()
+	{
+		return $this->hasOne('Pitch');
+	}
+
+	/**
+	 * Register event listener to clear cache on model save
+	 */
+	public static function boot()
+	{
+		parent::boot();
+
+		self::saving(function($story) {
+			\Cache::forget("story-{$story->id}");
+		});
+	}
+
+	public function getContent()
+	{
+		$content = Cache::rememberForever("story-{$this->id}", function() {
+			$blurb = MarkdownExtra::defaultTransform($this->blurb);
+			$blurb = Smartypants::defaultTransform($blurb);
+			$body = MarkdownExtra::defaultTransform($this->body);
+			$body = Smartypants::defaultTransform($body);
+			$title = Smartypants::defaultTransform($this->title);
+			return ['title' => $title, 'body' => $body,
+					'blurb' => $this->blurb];
+		});
+		return $content;
+	}
 
 }
