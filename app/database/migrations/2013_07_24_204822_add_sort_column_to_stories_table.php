218cf581084aad3500117ffd23d307ecd9cb0e09
Watts Martin <layotl@gmail.com>
2013-07-24 20:55:36 -0700

    Add sort column to Story

---
+++ app/database/migrations/2013_07_24_204822_add_sort_column_to_stories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddSortColumnToStoriesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('stories', function(Blueprint $table) {
+            $table->integer('sort')->unsigned()->default(1);
+            $table->unique(['sort', 'issue_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('stories', function(Blueprint $table) {
+        	$table->dropIndex('stories_sort_issue_id_unique');
+        	$table->dropColumn('sort');
+        });
+    }
+
+}
