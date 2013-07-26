602b032a213b47ace46f04a4420f3b1b2ef15b4e
Watts Martin <layotl@gmail.com>
2013-07-25 18:53:01 -0700

    Tab tab tab tab dammit

--- app/routes.php
+++ app/routes.php
@@ -6,27 +6,23 @@ Route::group(['prefix' => 'sysop', 'before' => 'auth.basic'], function()
 {
 	// Index
 	Route::get('/', 'Admin\PitchesController@index');
-    // Route::get('/', function()
-    // {
-    //     return Redirect::route('sysop.pitches.index');
-    // });
 
-    Route::resource('pages', 'Admin\PagesController');
-    Route::resource('authors', 'Admin\AuthorsController');
-    Route::resource('stories', 'Admin\StoriesController');
-    Route::resource('issues', 'Admin\IssuesController');
-    Route::get('issues/publish/{id}', ['uses'=>'Admin\IssuesController@publish', 'as'=>'sysop.issues.publish']);
+	Route::resource('pages', 'Admin\PagesController');
+	Route::resource('authors', 'Admin\AuthorsController');
+	Route::resource('stories', 'Admin\StoriesController');
+	Route::resource('issues', 'Admin\IssuesController');
+	Route::get('issues/publish/{id}', ['uses'=>'Admin\IssuesController@publish', 'as'=>'sysop.issues.publish']);
 
-    // Pitches need to switch between showing all and pending
-    Route::get('pitches/{show?}', ['uses'=>'Admin\PitchesController@index', 'as'=>'sysop.pitches.index']);
-    Route::get('pitches/show/{id}', ['uses'=>'Admin\PitchesController@show', 'as'=>'sysop.pitches.show']);
-    Route::get('pitches/edit/{id}', ['uses'=>'Admin\PitchesController@edit', 'as'=>'sysop.pitches.edit']);
-    Route::put('pitches/update/{id}', ['uses'=>'Admin\PitchesController@update', 'as'=>'sysop.pitches.update']);
+	// Pitches need to switch between showing all and pending
+	Route::get('pitches/{show?}', ['uses'=>'Admin\PitchesController@index', 'as'=>'sysop.pitches.index']);
+	Route::get('pitches/show/{id}', ['uses'=>'Admin\PitchesController@show', 'as'=>'sysop.pitches.show']);
+	Route::get('pitches/edit/{id}', ['uses'=>'Admin\PitchesController@edit', 'as'=>'sysop.pitches.edit']);
+	Route::put('pitches/update/{id}', ['uses'=>'Admin\PitchesController@update', 'as'=>'sysop.pitches.update']);
 
-    // Temporary route used to do arbitrary things
-    Route::get('/do', function() {
-    	return '<pre>Environment: ' . App::environment() . "</pre>\n";
-    });
+	// Temporary route used to do arbitrary things
+	Route::get('/do', function() {
+		return '<pre>Environment: ' . App::environment() . "</pre>\n";
+	});
 });
 
 // Static page display
