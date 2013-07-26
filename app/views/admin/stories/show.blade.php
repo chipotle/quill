dcd39d4c68e4a75038db762ff5a0af59c3410e24
Watts Martin <layotl@gmail.com>
2013-07-25 18:02:26 -0700

    Improve story preview template

--- app/views/admin/stories/show.blade.php
+++ app/views/admin/stories/show.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.admin')
 
-@section('title') Story Preview @endsection
-
 @section('head')
   <script type="text/javascript" src="//use.typekit.net/fky8rov.js"></script>
   <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
@@ -9,22 +7,29 @@
 
 @section('content')
 
+<header class="preview">Preview: {{ $title }}</header>
+
 <div id="preview">
   <article>
-    <h1>{{ $story->title }}</h1>
+    <h1>{{ $title }}</h1>
 
-    @if ($story->subhead)
-    <p class="subhead">{{ $story->subhead }}</p>
+    @if ($subhead)
+    <p class="subhead">{{ $subhead }}</p>
     @endif
 
-    <p class="author">{{ $story->author->getPreferredName() }}</p>
+    <p class="author">{{ $author }}</p>
 
     <div class="excerpt">
-      {{ $story->blurb }}
+      {{ $blurb }}
     </div>
 
-    {{ $story->body }}
+    {{ $body }}
   </article>
 </div>
+
+<footer class="preview">
+  <a href="{{ URL::route('sysop.stories.index') }}" class="btn"><i class="icon-backward"></i> Back</a>
+  <a href="{{ URL::route('sysop.stories.edit', [$id]) }}" class="btn"><i class="icon-edit"></i> Edit</a>
+</footer>
 
 @endsection
