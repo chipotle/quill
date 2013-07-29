3270da08d543b2756fe7110435ec0642f63b851c
Watts Martin <layotl@gmail.com>
2013-07-28 22:40:04 -0700

    Add jQuery-powered new story function

--- app/views/admin/stories/new.blade.php
+++ app/views/admin/stories/new.blade.php
@@ -1,65 +1,78 @@
 @extends('layouts.admin')
 
-@section('title') New Author @endsection
+@section('title') New Story @endsection
 
 @section('content')
 
-{{ Form::model($author, array('route' => 'sysop.authors.store')) }}
-  <div>
-    {{ Form::label('name', 'Name:') }}
-    {{ Form::text('name') }}
+{{ Form::model($story, array('route' => 'sysop.stories.store')) }}
+
+  {{ Form::label('title', 'Title') }}
+  {{ Form::text('title', null, ['class' => 'input-xxlarge']) }}
+
+  {{ Form::label('subhead', 'Subhead') }}
+  {{ Form::text('subhead', null, ['class' => 'input-xxlarge', 'placeholder' => '(Optional)']) }}
+
+  <div id="author-control" class="control-group">
+  {{ Form::label('author', 'Author') }}
+  {{ Form::text('author', null, ['class' => 'input-xlarge', 'autocomplete' => 'off']) }}
   </div>
 
-  <div>
-    {{ Form::label('nickname', 'Nickname:') }}
-    {{ Form::text('nickname', null, ['placeholder' => '(Optional)']) }}
-  </div>
+  {{ Form::hidden('author_id', null, ['id' => 'author_id', 'data-return' => 'x']) }}
 
-  <div>
-    <label class="radio inline">
-      {{ Form::radio('show', Author::SHOW_NAME) }}
-      Show name
-    </label>
-    <label class="radio inline">
-      {{ Form::radio('show', Author::SHOW_NICK) }}
-      Show nickname
-    </label>
-    <label class="radio inline">
-      {{ Form::radio('show', Author::SHOW_BOTH) }}
-      Show both
-    </label>
-  </div>
-  <br>
+  {{ Form::label('slug', 'Slug (must be unique within issue)') }}
+  {{ Form::text('slug', null, ['class'=>'input-xxlarge']) }}
 
-  <div>
-    {{ Form::label('email', 'Email:') }}
-    {{ Form::email('email') }}
-  </div>
+  {{ Form::label('blurb', 'Blurb or Excerpt (Markdown)') }}
+  {{ Form::textarea('blurb', null, ['class'=>'input-block-level']) }}
 
-  <div>
-    {{ Form::label('website', 'Website:') }}
-    {{ Form::input('url', 'website', null, ['placeholder' => '(Optional)']) }}
-  </div>
+  {{ Form::label('body', 'Body (Markdown)') }}
+  {{ Form::textarea('body', null, ['class'=>'input-block-level long']) }}
 
-  <div>
-    {{ Form::label('twitter', 'Twitter:') }}
-    {{ Form::text('twitter', null, ['placeholder' => '(Optional)']) }}
-  </div>
-
-  <div>
-    {{ Form::label('bio', 'Bio:') }}
-    {{ Form::textarea('bio', null, ['class' => 'input-xxlarge']) }}
-  </div>
-
-  <div>
-    {{ Form::label('user_id', 'Existing Quill User?') }}
-    {{ Form::select('user_id', $users) }}
-  </div>
-
-  <div>
-      {{ Form::submit('Submit', ['class'=>'btn', 'id'=>'submit']) }}
-      {{ HTML::linkRoute('sysop.authors.index', 'Cancel', null, ['class'=>'btn btn-danger'])}}
-  </div>
+  <p>
+    {{ Form::submit('Submit', ['class'=>'btn', 'id'=>'submit']) }}
+    {{ HTML::linkRoute('sysop.pages.index', 'Cancel', null, ['class'=>'btn btn-danger'])}}
+</p>
 {{ Form::close() }}
 
 @endsection
+
+@section('scripts')
+<script>
+$(function() {
+  $('#author').typeahead({
+    source: function getTypeahead(query, process) {
+      return $.get('/sysop/authors/search', { term: query }, function(data) {
+        var keys = Object.keys(data);
+        if (keys.length == 1) {
+          var name = Object.keys(data)[0];
+          $('#author').val(name);
+          $("#author_id").val(data[name]);
+          $('#author-control').addClass('success');
+          $('#author-control').removeClass('error');
+        }
+        else {
+          $('#author_id').data('return', data);
+          $('#author_id').val('');
+        }
+        return process(keys);
+      });
+    }
+  });
+
+  $('#author').on('blur', function() {
+    var data = $('#author_id').data('return');
+    var name = $('#author').val();
+    if (data[name]) {
+      $('#author_id').val(data[name]);
+      $('#author-control').addClass('success');
+      $('#author-control').removeClass('error');
+    }
+    else {
+      $('#author-control').addClass('error');
+      $('#author-control').removeClass('success');
+    }
+  });
+});
+</script>
+@endsection
+
