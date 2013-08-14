f403c35efd14d559b8f8ca5fc3a63d74818ad241
Watts Martin <layotl@gmail.com>
2013-08-14 16:08:06 -0700

    Prettify story/pitch lists on author pages

--- app/views/admin/authors/show.blade.php
+++ app/views/admin/authors/show.blade.php
@@ -4,30 +4,59 @@
 
 @section('content')
 
-<a href='{{ URL::route("sysop.stories.createwith", [$author->id]) }}' class="btn btn-success" style="color:white"><i class="icon-plus icon-white"></i> New Story</a>
+<p>
+  {{ HTML::mailTo($author->email, 'Send Mail', ['class'=>'btn']) }}
+  <a href='{{ URL::route("sysop.stories.createwith", [$author->id]) }}' class="btn btn-success" style="color:white">New Story</a>
+</p>
 
 <div class="row">
+
   <div class="span6">
     <h2>Stories</h2>
-    <ul>
-      @foreach ($author->stories as $story)
-      <li>
-        {{ HTML::linkRoute('sysop.stories.show', $story->title, [$story->id]) }}
-        @if ($story->issue_id)
-        ({{ HTML::linkRoute('sysop.issues.show', 'Issue ' . $story->issue->volnum(), [$story->issue_id]) }})
-        @endif
-      </li>
-      @endforeach
-    </ul>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Issue</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($author->stories as $story)
+        <tr>
+          <td>{{ HTML::linkRoute('sysop.stories.show', $story->title, [$story->id]) }}</td>
+          <td>
+            @if ($story->issue_id)
+              {{ HTML::linkRoute('sysop.issues.show', 'Issue ' . $story->issue->volnum(), [$story->issue_id]) }}
+            @else
+              &mdash;
+            @endif
+          </td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
   </div>
+
   <div class="span6">
-    <h2>Pitches</h2>
-    <ul>
-      @foreach ($author->pitches as $pitch)
-      <li>{{ HTML::linkRoute('sysop.pitches.show', HTML::truncate($pitch->blurb), [$pitch->id]) }} ({{ Pitch::$statusList[$pitch->status] }})</li>
-      @endforeach
-    </ul>
+    <h2>Pending Pitches</h2>
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th>Blurb</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach ($author->pitches as $pitch)
+        <tr>
+          <td>{{ HTML::linkRoute('sysop.pitches.show', HTML::truncate($pitch->blurb), [$pitch->id]) }}</td>
+          <td>{{ Pitch::$statusList[$pitch->status] }}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
   </div>
+
 </div>
 
 @endsection
