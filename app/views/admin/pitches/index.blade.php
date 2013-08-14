94dcc49febdc85e03fa4b4271720f687b032bafc
Watts Martin <layotl@gmail.com>
2013-08-14 16:06:21 -0700

    Link to author from pitch view if available

--- app/views/admin/pitches/index.blade.php
+++ app/views/admin/pitches/index.blade.php
@@ -37,7 +37,11 @@ Pitches
       <td><a href='{{ URL::route("sysop.pitches.edit", [$pitch->id]) }}' title="Edit" class="btn"><i class="icon-edit"></i></a></td>
       <td>{{ $pitch->id }}</td>
       <td>{{ HTML::date_fmt($pitch->created_at) }}</td>
+      @if ($pitch->author_id)
+      <td><b>{{ HTML::linkRoute('sysop.authors.show', $pitch->author->name, [$pitch->author_id]) }}</b></td>
+      @else
       <td>{{ HTML::linkRoute('sysop.pitches.show', $pitch->name, [$pitch->id]) }}</td>
+      @endif
       <td>{{ HTML::mailto($pitch->email) }}</td>
       <td>{{ Pitch::$statusList[$pitch->status] }}</td>
       <td style="height:1em;overflow:hidden">{{ HTML::linkRoute('sysop.pitches.show', HTML::truncate($pitch->blurb), [$pitch->id]) }}</td>
