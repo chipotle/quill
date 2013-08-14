d4051c2c1eebeb4bf3e225bebe2957163fdb04e9
Watts Martin <layotl@gmail.com>
2013-08-14 10:51:17 -0700

    Start draggable/sortable UI for Issue TOCs

--- app/views/admin/issues/show.blade.php
+++ app/views/admin/issues/show.blade.php
@@ -4,24 +4,74 @@
 
 @section('content')
 
-<p>Status: {{ ($issue->published ? 'Published' : 'Unpublished') }}
+@if ($issue->published)
+
+<p><i>The contents cannot be edited once an issue is published.</i></p>
+
 <div class="row">
   <div class="span6">
     <h2>Stories</h2>
-    <ul>
+    <ol>
       @foreach ($issue->stories as $story)
-      <li>{{ $story->title }}</li>
+      <li><b>{{ $story->title }}</b> {{ $story->author->name }}</li>
+      @endforeach
+    </ol>
+  </div>
+</div>
+
+@else
+
+<p>
+  <button class="btn btn-primary" id="action">Publish</button>
+  <button class="btn btn-danger" id="revert" style="display:none">Revert</button>
+</p>
+
+<div class="row">
+  <div class="span6">
+    <h2>Stories</h2>
+    <ul id="toc" class="connectedSortable">
+      @foreach ($issue->stories as $story)
+      <li class="draggable"><b>{{ $story->title }}</b> {{ $story->author->name }}</li>
       @endforeach
     </ul>
   </div>
   <div class="span6">
     <h2>Unassigned Stories</h2>
-    <ul>
+    <ul id="unassigned" class="connectedSortable">
       @foreach ($unassigned as $story)
-      <li>{{ $story->title }}</li>
+      <li class="draggable"><b>{{ $story->title }}</b> {{ $story->author->name }}</li>
       @endforeach
     </ul>
   </div>
 </div>
 
+@endif
+
+@endsection
+
+@section('scripts')
+@if ( ! $issue->published)
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+<script>
+$(function() {
+  $('#toc, #unassigned').sortable({
+    connectWith: '.connectedSortable',
+    update: function(event, ui) {
+      $('#action').removeClass('btn-primary').addClass('btn-warning').text('Commit');
+      $('#action').removeAttr('disabled');
+      $('#revert').show();
+    }
+  }).disableSelection();
+  $('#revert').on('click', function() { location.reload(); })
+  $('#action').on('click', function() {
+    var type = $(this).text();
+    alert(type);
+    return false;
+  })
+  if ($('#toc li').length == 0) {
+    $('#action').attr('disabled', 'disabled');
+  }
+});
+</script>
+@endif
 @endsection
