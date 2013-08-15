3653cf96fc732f00c4a6ca60f07dfac3bebbf056
Watts Martin <layotl@gmail.com>
2013-08-14 22:37:41 -0700

    Add test for getContent() method

--- app/tests/StoryTest.php
+++ app/tests/StoryTest.php
@@ -1,4 +1,5 @@
 <?php
+use Way\Tests\Factory;
 
 class StoryTest extends TestCase {
 	use Way\Tests\ModelHelpers;
@@ -16,6 +17,29 @@ class StoryTest extends TestCase {
     public function testHasOnePitch()
     {
     	$this->assertHasOne('pitch', 'Story');
+    }
+
+    public function testGetContent()
+    {
+    	Cache::forget('story-x');
+    	$author = Factory::make('author');
+    	$author->show = Author::SHOW_NICK;
+    	$story = Factory::make('story', [
+    		'author' => $author,
+			'id' => 'x',
+			'blurb' => 'This is a "thing"',
+	   		'body' => "Lorem _ipsum_ dolor amet\n\nLorem--ipsum",
+	   		'title' => "Big 'thing'",
+    	]);
+    	$return = [
+    		'blurb' => "<p>This is a &#8220;thing&#8221;</p>\n",
+    		'body' => "<p>Lorem <em>ipsum</em> dolor amet</p>\n\n<p>Lorem&#8212;ipsum</p>\n",
+    		'title' => "Big &#8216;thing&#8217;",
+    		'author' => $author->nickname,
+    		'subhead' => $story->subhead,
+    		'id' => 'x'
+    	];
+    	$this->assertEquals($return, $story->getContent());
     }
 
 }
