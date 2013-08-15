219f2f051695eedfa86c063370239a56237f9cbe
Watts Martin <layotl@gmail.com>
2013-08-14 22:37:59 -0700

    Add Issue model tests

---
+++ app/tests/IssueTest.php
@@ -0,0 +1,19 @@
+<?php
+use Way\Tests\Factory;
+
+class IssueTest extends TestCase {
+	use Way\Tests\ModelHelpers;
+
+    public function testHasManyStories()
+    {
+    	$this->assertHasMany('stories', 'Issue');
+    }
+
+    public function testVolNum()
+    {
+        $issue = Factory::make('issue');
+        $expected = (Config::get('quill.use_volumes') ? "{$issue->volume}.{$issue->number}" : $issue->number);
+        $this->assertEquals($expected, $issue->volnum());
+    }
+
+}
