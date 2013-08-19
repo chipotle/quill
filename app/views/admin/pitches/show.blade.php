2f561ed631a1a99b47582be87e3426dbec34e221
Watts Martin <layotl@gmail.com>
2013-08-19 12:48:06 -0700

    Improve pitch metadata linking

--- app/views/admin/pitches/show.blade.php
+++ app/views/admin/pitches/show.blade.php
@@ -6,13 +6,21 @@
 
 <dl class="dl-horizontal">
   <dt>From</dt>
+  @if ($pitch->author_id)
+  <dd> {{ HTML::linkRoute('sysop.authors.show', $pitch->author->getPreferredName(), [$pitch->author->id]) }} &lt;{{ HTML::mailto($pitch->author->email) }}&gt;</dd>
+  @else
   <dd>{{ $pitch->name }} &lt;{{ HTML::mailto($pitch->email) }}&gt;</dd>
+  @endif
   <dt>Submitted</dt>
-  <dd>{{ HTML::date_fmt($pitch->created_at, 'd-M-y H:i') }}</dd>
+  <dd>{{ $pitch->created_at->toFormattedDateString() }}</dd>
   <dt>Updated</dt>
-  <dd>{{ HTML::date_fmt($pitch->updated_at, 'd-M-y H:i') }}</dd>
+  <dd>{{ $pitch->updated_at->toFormattedDateString() }}</dd>
   <dt>Status</dt>
+  @if ($pitch->story_id)
+  <dd>{{ HTML::linkRoute('sysop.stories.show', Pitch::$statusList[$pitch->status], [$pitch->story_id]) }}</dd>
+  @else
   <dd>{{ Pitch::$statusList[$pitch->status] }}</dd>
+  @endif
 </dl>
 
 <div class="well" style="white-space:pre-line">{{ $pitch->blurb }}</div>
