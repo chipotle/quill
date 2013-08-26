e1d5190b9e278f597bc58eaf61c0525695b2b7cc
Watts Martin <layotl@gmail.com>
2013-08-26 12:28:14 -0700

    Add jQuery footnote popups

--- app/views/page.blade.php
+++ app/views/page.blade.php
@@ -8,4 +8,6 @@
 <article class="page">
 {{ $body }}
 </article>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+{{ HTML::script('js/footnote.js') }}
 @endsection
