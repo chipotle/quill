56980032a39c2ad93eeab99c7cce29783eb93c75
Watts Martin <layotl@gmail.com>
2013-10-17 23:12:39 -0700

    Add feed to cover page

--- app/views/cover.blade.php
+++ app/views/cover.blade.php
@@ -4,6 +4,10 @@
 Claw &amp; Quill
 @endsection
 
+@section('head')
+<link href="{{ URL::action('HomeController@feed') }}" type="application/atom+xml" rel="alternate" title="Syndication Feed">
+@endsection
+
 @section('content')
 <header id="header">
   <div><img src="/img/cnq-logo.png" alt="Claw &amp; Quill"></div>
@@ -21,8 +25,8 @@ Claw &amp; Quill
 </article>
 
 <footer class="toc">
-  <p class="pull-right"><a href="http://twitter.com/clawandquill">Twitter</a></p>
+  <p class="pull-right"><a href="http://twitter.com/clawandquill">Twitter</a> &middot; {{ HTML::linkAction('HomeController@feed', 'Feed') }}</p>
   <p>{{ HTML::linkRoute('page', 'About C&amp;Q', ['about']) }} &middot; {{ HTML::linkRoute('page', 'Submission Guidelines', ['guidelines']) }} &middot; {{ HTML::linkAction('IssueController@getIndex', 'Issue Index') }} &middot; {{ HTML::linkRoute('page', 'Colophon', ['colophon']) }}</p>
-  <p>Copyright 2013 Claw &amp; Quill &middot; <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" title="Creative Commons Attribution-NonCommercial-ShareAlike 3.0">CC BY-NC-SA</a></p>
+  <p>{{ HTML::linkRoute('page', 'Copyright', ['copyright']) }} 2013 Claw &amp; Quill &middot; <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/" title="Creative Commons Attribution-NonCommercial-ShareAlike 3.0">CC BY-NC-SA</a></p>
 </footer>
 @endsection
