7f0ec7de9f30c10e178d7007089ebf7bc4dc0069
Watts Martin <layotl@gmail.com>
2013-10-20 22:11:34 -0700

    Refactor jQuery delete confirmation to be reusable

--- app/views/admin/pages/index.blade.php
+++ app/views/admin/pages/index.blade.php
@@ -18,7 +18,7 @@
     <tr>
       <td style="width:10%;white-space:nowrap">
         <a href='{{ URL::route("sysop.pages.edit", [$page->id]) }}' title="Edit" class="btn"><i class="icon-edit"></i></a>
-        <a href='{{ URL::route("sysop.pages.destroy", [$page->id]) }}' title="Delete" class="btn btn-danger delete" data-page="{{ $page->slug }}"><i class="icon-remove icon-white"></i></a>
+        <a href='{{ URL::route("sysop.pages.destroy", [$page->id]) }}' title="Delete" class="btn btn-danger delete" data-name="{{ $page->slug }}"><i class="icon-remove icon-white"></i></a>
       </td>
       <td style="vertical-align:middle">{{ HTML::linkRoute('sysop.pages.show', $page->slug, [$page->id]) }}</td>
       <td style="vertical-align:middle">{{ HTML::linkRoute('sysop.pages.show', $page->title, [$page->id]) }}</td>
@@ -35,34 +35,4 @@
 </table>
 
 <a href='{{ URL::route("sysop.pages.create") }}' class="btn btn-success" style="color:white"><i class="icon-plus icon-white"></i> New</a>
-@endsection
-
-@section('scripts')
-<script>
-$(function(){
-  $('.delete').on('click', function(event) {
-    var href = $(this).attr('href');
-    var page = $(this).data('page');
-    if (confirm('Delete "' + page + '"?')) {
-      $.ajax({
-        url: href,
-        type: 'delete',
-        dataType: 'json',
-        success: function(response) {
-          if (response.alert) {
-            alert(response.alert);
-          }
-          if (response.reload) {
-            window.location.reload(true);
-          }
-          else if (response.redirect) {
-            window.location.replace(response.redirect);
-          }
-        }
-      });
-    }
-    return false;
-  });
-});
-</script>
 @endsection
--- js/admin.js
+++ js/admin.js
@@ -4,3 +4,29 @@ function makeSlug(slug) {
 	slug = slug.replace(/-{2,}/g, '-');
 	return slug;
 }
+
+$(function(){
+  $('.delete').on('click', function(event) {
+    var href = $(this).attr('href');
+    var name = $(this).data('name');
+    if (confirm('Delete "' + name + '"?')) {
+      $.ajax({
+        url: href,
+        type: 'delete',
+        dataType: 'json',
+        success: function(response) {
+          if (response.alert) {
+            alert(response.alert);
+          }
+          if (response.reload) {
+            window.location.reload(true);
+          }
+          else if (response.redirect) {
+            window.location.replace(response.redirect);
+          }
+        }
+      });
+    }
+    return false;
+  });
+});
