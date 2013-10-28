710b8cf7c9521cdc0af8d3af699e5c0fc0c4fbd2
Watts Martin <layotl@gmail.com>
2013-10-28 12:00:30 -0700

    Fix author index query clause

--- app/controllers/AuthorController.php
+++ app/controllers/AuthorController.php
@@ -17,7 +17,7 @@ class AuthorController extends BaseController {
 	 */
 	function getIndex()
 	{
-		$authors = DB::table('authors')->select('authors.id', 'authors.name', 'authors.show', 'authors.nickname', 'authors.email', DB::raw('count(stories.id) as story_count'), DB::raw('CASE WHEN `show`='.Author::SHOW_NICK.' THEN nickname ELSE name END as \'preferred\''))->join('stories', 'authors.id', '=', 'stories.author_id')->where('stories.issue_id', '>', '0')->orderBy('preferred', 'asc')->paginate(30);
+		$authors = DB::table('authors')->select('authors.id', 'authors.name', 'authors.show', 'authors.nickname', 'authors.email', DB::raw('CASE WHEN `show`='.Author::SHOW_NICK.' THEN nickname ELSE name END as \'preferred\''))->join('stories', 'authors.id', '=', 'stories.author_id')->distinct()->where('stories.issue_id', '>', '0')->orderBy('preferred', 'asc')->paginate(30);
 		$stories = [];
 		$names = [];
 		foreach ($authors as $author) {
