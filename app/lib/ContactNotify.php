e662d83b9bd4c6e973a3b6aeb7533c2974e81471
Watts Martin <layotl@gmail.com>
2013-10-30 00:14:16 -0700

    Handle blank replyTo in contact email

--- app/lib/ContactNotify.php
+++ app/lib/ContactNotify.php
@@ -5,10 +5,11 @@ class ContactNotify {
 	public function fire($job, $data)
 	{
 		Mail::send(['text' => 'emails.contact'], $data, function($m) use ($data) {
+			$reply_to = $data['email'] ?: Config::get('quill.send.email');
 			$m->to(Config::get('quill.contact.email'), Config::get('quill.contact.name'))
 				->subject("[cnq] Contact from {$data['name']}")
 				->from(Config::get('quill.send.email'), Config::get('quill.send.name'))
-				->replyTo($data['email']);
+				->replyTo($reply_to);
 		});
 	}
 }
